c6c79ef8454c453207f8168eeebad9a624d9eda5
KarabasBurak <117377125+KarabasBurak@users.noreply.github.com>
2024-06-10 13:47:13 +0300

    Angular_DataBinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  //templateUrl: './app.component.html',
+  template: `
+    <!-- // Property Binding
+    <input type="text" [value]="title" />
+    <app-home [pageName]="title"> </app-home>
+
+    // Two-Way Data Binding
+    <input type="text" [(ngModel)]="name" /> -->
+  `,
+  styleUrl: './app.component.scss',
+  imports: [RouterOutlet, CommonModule, HomeComponent, FormsModule],
 })
 export class AppComponent {
   title = 'proje1';
+  name: string;
 }
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  //templateUrl: './home.component.html',
+  template: ` <h1>{{ pageName }}</h1> `,
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent {
+  @Input() pageName: string;
+}
